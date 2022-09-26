07a87fcd87b150be6290cbc58bfee77d65395ed3
Konstantinos <kon.xen@:gmail.com>
2022-09-26 13:13:35 +0100

    Create book functionality with author as field Co-authored-by: JackHughes <JackHughes90@users.noreply.github.com>

--- Bookish/Controllers/BookController.cs
+++ Bookish/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Bookish.Models;
+using Bookish.Models.Requests;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookish.Controllers;
@@ -23,5 +24,51 @@ public class BookController : Controller
             .ToList();
 
         return View(books);
+    }
+
+    [HttpPost("")]
+    public IActionResult Create([FromForm] CreateBookRequest newBookRequest)
+    {
+        var context = new BookishContext();
+
+        // Make new authors (quick and dirty - will duplicate authors of same name)
+        List<Author> newBookAuthors = new List<Author>();
+        foreach (string authorName in newBookRequest.Authors)
+        {
+            Author? existingAuthor = context.Authors.Where(a => a.Name == authorName).SingleOrDefault();
+            if (existingAuthor is not null)
+            {
+                newBookAuthors.Add(existingAuthor);
+                break;
+            }
+
+            Author newAuthor = new Author()
+            {
+                Name = authorName
+            };
+            var insertedAuthorEntity = context.Add(newAuthor);
+            newBookAuthors.Add(insertedAuthorEntity.Entity);
+        }
+
+        Book newBook = new Book
+        {
+            Title = newBookRequest.Title,
+            Image = newBookRequest.Image,
+            Authors = newBookAuthors,
+        };
+
+        var addedEntity = context.Books.Add(newBook);
+
+        context.SaveChanges();
+
+        Book addedBook = addedEntity.Entity;
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet("create")]
+    public IActionResult BookForm()
+    {
+        return View();
     }
 }
